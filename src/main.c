14ae7d65f351b06ae636ddb8d49ca5bac1ec29f7
Lokesh Dhakad <ldhakad2005@gmail.com>
2026-03-24 22:33:44 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
   char input[100];
   fgets(input, sizeof(input), stdin);
 
-  input[strlen(input) - 1] = "\0";
+  input[strlen(input) - 1] = '\0';
   printf("%s:command not found\n", input);
   return 0;
 }
