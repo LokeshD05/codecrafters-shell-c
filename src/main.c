96f77695e3673d836d13bb894c95c99c9819f779
Lokesh Dhakad <ldhakad2005@gmail.com>
2026-03-25 21:05:41 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
         printf("%s is a shell builtin\n", cmd);
       }
       else{
-        printf("%s :not found\n");
+        printf("%s :not found\n",cmd);
       }
       else
       {
