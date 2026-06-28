d8063e14fe8517767100faa6e4bcd35dc5985bde
Lokesh Dhakad <ldhakad2005@gmail.com>
2026-06-28 22:03:58 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -181,7 +181,7 @@ int main(int argc, char *argv[])
 
     int pipe_idx = -1;
     for(int i = 0;i<argc;i++){
-      if(argumenst[i] == '|'){
+      if(arguments[i] == '|'){
         pipe_idx = i;
         break;
       }
